8224cb0aa855a2193b7df5f54f3bf7b6bf1039bc
JRivera1313 <46766747+JRivera1313@users.noreply.github.com>
2019-04-10 16:26:23 -0500

    IT WORKS

--- binomial.c
+++ binomial.c
@@ -58,24 +58,21 @@ long chooseWithMemoizationRecursive(int n, int k, long **tableau) {
   numCalls++;
   long value = -1;
 
-  //implement error checking on invalid n and k
+  //implement error checking on invalid n and k                                 //JR
   if(k < 0 || n < 0) {                                                          //JR
-    printf("invalid inputs: choose(%d, %d), quitting on you...\n", n, k);
-    exit(1);
-  }
+    printf("invalid inputs: choose(%d, %d), quitting on you...\n", n, k);       //JR
+    exit(1);                                                                    //JR
+  }                                                                             //JR
 
-  //if the value has already been computed, return it...
+  //if the value has already been computed, return it...                        //JR
   if (tableau[n][k] != -1) {                                                    //JR, Checks if the value has been computed yet, and returns it
-    value = tableau[n][k];
-  }
+    value = tableau[n][k];                                                      //JR
+    return value;                                                               //JR
+  }                                                                             //JR
 
-  //Chooses how much recusion to do.
-  if (value != -1) {
-    return value;
-  }
-  else{
-    value = chooseWithMemoizationRecursive(n - 1, k,tableau) + chooseWithMemoizationRecursive(n - 1, k - 1,tableau); //JR, yay recursion :|....                                                          //JR, pretty much the same code from Choose
-  }
+    tableau[n][k] = chooseWithMemoizationRecursive(n - 1, k,tableau) + chooseWithMemoizationRecursive(n - 1, k - 1,tableau); //JR, Stores the value in Tablue.
+
+  value = tableau[n][k];                                                        //JR, Value was was part of the starter code so, I used it.
   return value;
 }
 
@@ -86,15 +83,18 @@ long choose(int n, int k) {
 
   //implement error checking on invalid n and k
   if(k < 0 || n < 0) {                                                          //JR
-    printf("invalid inputs: choose(%d, %d), quitting on you...\n", n, k);
-    exit(1);
+    printf("invalid inputs: choose(%d, %d), quitting on you...\n", n, k);       //JR
+    exit(1);                                                                    //JR
   }
 
   //implement Pascal's Rule to choose
+  //TEMP CODE TO HELLP FIGURE THE DAMN THING OUT//
+  printf("Choose %d %d \t %d\n",n,k,numCalls);
 
   //Base case
   if (k == 0 || k == n) {                                                       //JR
     return 1;                                                                   //JR
-  }                                                                             //JR
+  }
+
   return choose(n - 1, k) + choose(n - 1, k - 1);                               //JR
 }
