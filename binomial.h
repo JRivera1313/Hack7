bf238437cc63820ce1e2ebec19d8596060ca97e2
JRivera1313 <46766747+JRivera1313@users.noreply.github.com>
2019-04-10 16:54:21 -0500

    Added documetion and Freed Malloc'ed memory

--- binomial.h
+++ binomial.h
@@ -12,11 +12,13 @@ static long numCalls = 0;
 //returns the number of function calls that were necessary to execute Pascal's Rule in choosing
 long getNumCalls();
 
-//TO DOCUMENT
+//Returns N choose K by the recursive formula (n, k) = (n-1, k) + (n-1, k -1)
 long choose(int n, int k);
 
-//Allocates memory for its subsequent recusive call of chooseWithMemoizationRecursive
+//Allocates memory for its subsequent recusive call of chooseWithMemoizationRecursive and sets base cases
 long chooseWithMemoization(int n, int k);
 
-//TO DOCUMENT
+//Optomised version of the choose function. Preforms the same recusion. However, it stores all values per iteration in a table
+// that it then checks aginst. If the value is in the table, it ceses recurion and returns the areadly solved value in the table
+// instead.
 long chooseWithMemoizationRecursive(int n, int k, long **tableau);
